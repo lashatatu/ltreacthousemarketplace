e657eace35ed79a44123ef8e6cdcfb0cca048985
lasha tatulashvili <lashatatu@gmail.com>
2022-07-28 18:00:03 +0400

    section 13 end

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Explore from './pages/Explore'
+import Offers from './pages/Offers'
+import Profile from './pages/Profile'
+import SignIn from './pages/SignIn'
+import SingUp from './pages/SingUp'
+import ForgotPassword from './pages/ForgotPassword'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
-    <div>
-lasa
-    </div>
+    <>
+      <Router>
+        <Navbar>
+        <Routes>
+          <Route path={'/'} element={<Explore/>}/>
+          <Route path={'/offers'} element={<Offers/>}/>
+          <Route path={'/profile'} element={<Profile/>}/>
+          <Route path={'/sing-in'} element={<SignIn/>}/>
+          <Route path={'/sing-up'} element={<SingUp/>}/>
+          <Route path={'/forgot-password'} element={<ForgotPassword/>}/>
+        </Routes>
+        </Navbar>
+      </Router>
+
+    </>
   );
 }
 
