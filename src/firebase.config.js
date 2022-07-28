7b348ffa8b15c9c18dfe2bc2821f5115b216f709
lasha tatulashvili <lashatatu@gmail.com>
2022-07-28 17:42:41 +0400

    fist comit

---
+++ src/firebase.config.js
@@ -0,0 +1,14 @@
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyD9pu6UhgBjdCkgXLV-aZM9yKlnds632wU',
+  authDomain: 'lt-house-marketplace.firebaseapp.com',
+  projectId: 'lt-house-marketplace',
+  storageBucket: 'lt-house-marketplace.appspot.com',
+  messagingSenderId: '922877899185',
+  appId: '1:922877899185:web:4a98166d0d86e3a7040d90',
+}
+
+export const db = getFirestore();
