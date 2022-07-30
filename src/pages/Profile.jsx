02105e7ad15d342dc40a4928b03db00f037fd6ac
lasha tatulashvili <lashatatu@gmail.com>
2022-07-30 16:15:59 +0400

    section 14 display update user details 7

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,14 +1,30 @@
 import { getAuth } from 'firebase/auth'
 import { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Profile = () => {
-  const [user,setUser]=useState(null)
   const auth = getAuth()
-  useEffect(() => {
-    setUser(auth.currentUser)
-  }, [])
 
-  return user ? <h1>{user.displayName}</h1> :'not logged in'
+  const [formData, useFormData] = useState({
+    name: auth.currentUser.displayName,
+    email: auth.currentUser.email,
+  })
+
+
+  const navigate=useNavigate()
+  const onLogout=()=>{
+    auth.signOut()
+    navigate('/')
+  }
+
+  return <div className='profile'>
+    <header className='profileHeader'>
+      <p className='pageHeader'>My Profile</p>
+      <button type='button' className='logOut' onClick={onLogout}>
+        Logout
+      </button>
+    </header>
+  </div>
 }
 
 export default Profile
