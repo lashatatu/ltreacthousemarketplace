87987b1b827bd2bb6e4964f965961efe5a1c7ad9
lasha <lashatatu@gmail.com>
2022-10-23 23:22:18 +0400

    section 15 offers page 4

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -1,5 +1,5 @@
-import { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 import {
   collection,
   getDocs,
@@ -8,50 +8,51 @@ import {
   orderBy,
   limit,
   startAfter,
-} from 'firebase/firestore'
-import { db } from '../firebase.config'
-import { toast } from 'react-toastify'
-import Spinner from '../components/Spinner'
+} from 'firebase/firestore';
+import { db } from '../firebase.config';
+import { toast } from 'react-toastify';
+import Spinner from '../components/Spinner';
+import ListingItem from '../components/ListingItem';
 
 const Category = () => {
-  const [listings, setListings] = useState(null)
-  const [loading, setLoading] = useState(true)
+  const [listings, setListings] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-  const params = useParams()
+  const params = useParams();
 
   useEffect(() => {
     const fetchListings = async () => {
       try {
         // Get reference
-        const listingsRef = collection(db, 'listings')
+        const listingsRef = collection(db, 'listings');
 
         const q = query(
           listingsRef,
           where('type', '==', params.categoryName),
           orderBy('timestamp', 'desc'),
-          limit(10)
-        )
+          limit(10),
+        );
 
-        const querySnap = await getDocs(q)
+        const querySnap = await getDocs(q);
 
-        const listings = []
+        const listings = [];
 
         querySnap.forEach((doc) => {
           return listings.push({
             id: doc.id,
             data: doc.data(),
-          })
-        })
+          });
+        });
 
-        setListings(listings)
-        setLoading(false)
+        setListings(listings);
+        setLoading(false);
       } catch ( error ) {
-        toast.error('couldnt fetch listings')
+        toast.error('couldnt fetch listings');
       }
-    }
+    };
 
-    fetchListings()
-  }, [params.categoryName])
+    fetchListings();
+  }, [params.categoryName]);
 
   return (
     <div className='category'>
@@ -64,25 +65,23 @@ const Category = () => {
       </header>
 
       {loading ? (
-        <Spinner/>
+        <Spinner />
       ) : listings && listings.length > 0 ? (
         <>
-        <main>
-          <ul className='categoryListings'>
-            {listings.map((listing) =>
-            <h3>
-              {listing.data.name}
-            </h3>
-            )}
-          </ul>
-        </main>
+          <main>
+            <ul className='categoryListings'>
+              {listings.map((listing) =>
+                <ListingItem listing={listing.data} id={listing.id} key={listing.id} />,
+              )}
+            </ul>
+          </main>
         </>
       ) : (
         <p>No Listings for {params.categoryName}</p>
       )}
     </div>
-  )
-}
+  );
+};
 
-export default Category
+export default Category;
 
