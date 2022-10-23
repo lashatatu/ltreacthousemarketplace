0185a722caa63a10e82937763b18185aedfa9580
lasha <lashatatu@gmail.com>
2022-10-23 23:47:41 +0400

    section 15 create listing form

--- src/App.js
+++ src/App.js
@@ -1,35 +1,38 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import { ToastContainer } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css'
-import Explore from './pages/Explore'
-import Offers from './pages/Offers'
-import Profile from './pages/Profile'
-import SignIn from './pages/SignIn'
-import SingUp from './pages/SingUp'
-import ForgotPassword from './pages/ForgotPassword'
-import Navbar from './components/Navbar'
-import PrivateRoute from './components/PrivateRoute'
-import Category from './pages/Category'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Explore from "./pages/Explore";
+import Offers from "./pages/Offers";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SingUp from "./pages/SingUp";
+import ForgotPassword from "./pages/ForgotPassword";
+import Navbar from "./components/Navbar";
+import PrivateRoute from "./components/PrivateRoute";
+import Category from "./pages/Category";
+import CreateListing from "./pages/CreateListing";
 
-function App () {
-  return (<>
+function App() {
+  return (
+    <>
       <Router>
         <Routes>
-          <Route path={'/'} element={<Explore/>}/>
-          <Route path={'/offers'} element={<Offers/>}/>
-          <Route path={'/category/:categoryName'} element={<Category/>}/>
-          <Route path={'/profile'} element={<PrivateRoute/>}>
-            <Route path={'/profile'} element={<Profile/>}/>
+          <Route path={"/"} element={<Explore />} />
+          <Route path={"/offers"} element={<Offers />} />
+          <Route path={"/category/:categoryName"} element={<Category />} />
+          <Route path={"/profile"} element={<PrivateRoute />}>
+            <Route path={"/profile"} element={<Profile />} />
           </Route>
-          <Route path={'/sing-in'} element={<SignIn/>}/>
-          <Route path={'/sing-up'} element={<SingUp/>}/>
-          <Route path={'/forgot-password'} element={<ForgotPassword/>}/>
+          <Route path={"/sing-in"} element={<SignIn />} />
+          <Route path={"/sing-up"} element={<SingUp />} />
+          <Route path={"/forgot-password"} element={<ForgotPassword />} />
+          <Route path={"/create-listing"} element={<CreateListing />} />
         </Routes>
-        <Navbar/>
+        <Navbar />
       </Router>
-      <ToastContainer/>
-
-    </>)
+      <ToastContainer />
+    </>
+  );
 }
 
-export default App
+export default App;
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,98 +1,106 @@
-import { useState } from 'react'
-import { getAuth, updateProfile } from 'firebase/auth'
-import { updateDoc, doc } from 'firebase/firestore'
-import { db } from '../firebase.config'
-import { useNavigate } from 'react-router-dom'
-import { toast } from 'react-toastify'
+import { useState } from "react";
+import { getAuth, updateProfile } from "firebase/auth";
+import { updateDoc, doc } from "firebase/firestore";
+import { db } from "../firebase.config";
+import { Link, useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import arrowRight from "../assets/svg/keyboardArrowRightIcon.svg";
+import homeIcon from "../assets/svg/homeIcon.svg";
 
 function Profile() {
-  const auth = getAuth()
-  const [changeDetails, setChangeDetails] = useState(false)
+  const auth = getAuth();
+  const [changeDetails, setChangeDetails] = useState(false);
   const [formData, setFormData] = useState({
     name: auth.currentUser.displayName,
     email: auth.currentUser.email,
-  })
+  });
 
-  const { name, email } = formData
+  const { name, email } = formData;
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const onLogout = () => {
-    auth.signOut()
-    navigate('/')
-  }
+    auth.signOut();
+    navigate("/");
+  };
 
   const onSubmit = async () => {
     try {
       if (auth.currentUser.displayName !== name) {
         await updateProfile(auth.currentUser, {
           displayName: name,
-        })
+        });
 
-        const userRef = doc(db, 'users', auth.currentUser.uid)
+        const userRef = doc(db, "users", auth.currentUser.uid);
         await updateDoc(userRef, {
           name,
-        })
+        });
       }
     } catch (error) {
-      console.log(error)
-      toast.error('Could not update profile details')
+      console.log(error);
+      toast.error("Could not update profile details");
     }
-  }
+  };
 
   const onChange = (e) => {
     setFormData((prevState) => ({
       ...prevState,
       [e.target.id]: e.target.value,
-    }))
-  }
+    }));
+  };
 
   return (
-    <div className='profile'>
-      <header className='profileHeader'>
-        <p className='pageHeader'>My Profile</p>
-        <button type='button' className='logOut' onClick={onLogout}>
+    <div className="profile">
+      <header className="profileHeader">
+        <p className="pageHeader">My Profile</p>
+        <button type="button" className="logOut" onClick={onLogout}>
           Logout
         </button>
       </header>
 
       <main>
-        <div className='profileDetailsHeader'>
-          <p className='profileDetailsText'>Personal Details</p>
+        <div className="profileDetailsHeader">
+          <p className="profileDetailsText">Personal Details</p>
           <p
-            className='changePersonalDetails'
+            className="changePersonalDetails"
             onClick={() => {
-              changeDetails && onSubmit()
-              setChangeDetails((prevState) => !prevState)
+              changeDetails && onSubmit();
+              setChangeDetails((prevState) => !prevState);
             }}
           >
-            {changeDetails ? 'done' : 'change'}
+            {changeDetails ? "done" : "change"}
           </p>
         </div>
 
-        <div className='profileCard'>
+        <div className="profileCard">
           <form>
             <input
-              type='text'
-              id='name'
-              className={!changeDetails ? 'profileName' : 'profileNameActive'}
+              type="text"
+              id="name"
+              className={!changeDetails ? "profileName" : "profileNameActive"}
               disabled={!changeDetails}
               value={name}
               onChange={onChange}
             />
             <input
-              type='text'
-              id='email'
-              className={!changeDetails ? 'profileEmail' : 'profileEmailActive'}
+              type="text"
+              id="email"
+              className={!changeDetails ? "profileEmail" : "profileEmailActive"}
               disabled={!changeDetails}
               value={email}
               onChange={onChange}
             />
           </form>
         </div>
+
+        <Link to={"/create-listing"} className={"createListing"}>
+          <img src={homeIcon} alt="home" />
+          <p>Sell or rent your home</p>
+          <img src={arrowRight} alt="arrow right" />
+        </Link>
       </main>
     </div>
-  )
+  );
 }
 
-export default Profile
+export default Profile;
