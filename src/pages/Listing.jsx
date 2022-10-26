2b1171663fe424cc32f29d3fb02950623c5747a3
lasha <lashatatu@gmail.com>
2022-10-26 20:14:27 +0400

    16 section 3. contact landlord page

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -4,7 +4,7 @@ import { getDoc, doc } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 import { db } from "../firebase.config";
 import Spinner from "../components/Spinner";
-import share from "../assets/svg/shareIcon.svg";
+import shareIcon from "../assets/svg/shareIcon.svg";
 
 const Listing = () => {
   const [listing, setListing] = useState(null);
@@ -30,10 +30,77 @@ const Listing = () => {
     fetchListing();
   }, [navigate, params.listingId]);
 
+  if (loading) {
+    return <Spinner />;
+  }
+
   return (
-    <div>
-      <p>Listing</p>
-    </div>
+    <main>
+      {/*slider*/}
+      <div
+        className="shareIconDiv"
+        onClick={() => {
+          navigator.clipboard.writeText(window.location.href);
+          setShareLinkCopied(true);
+          setTimeout(() => {
+            setShareLinkCopied(false);
+          }, 2000);
+        }}
+      >
+        <img src={shareIcon} alt="" />
+      </div>
+
+      {shareLinkCopied && <p className={"linkCopied"}>Link Copied!</p>}
+      <div className="listingDetails">
+        <p className="listingName">
+          {listing.name} - $
+          {listing.offer
+            ? listing.discountedPrice
+                .toString()
+                .replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+            : listing.regularPrice
+                .toString()
+                .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+        </p>
+        <p className={"listingLocation"}>{listing.location}</p>
+        <p className={"listingType"}>
+          For {listing.type === "rent" ? "Rent" : "sale"}
+        </p>
+        {listing.offer && (
+          <p className="discountPrice">
+            ${listing.regularPrice - listing.discountedPrice} discount
+          </p>
+        )}
+
+        <ul className="listingDetailsList">
+          <li>
+            {listing.bedrooms > 1
+              ? `${listing.bedrooms} bedrooms`
+              : `${listing.bedrooms} bedroom`}
+          </li>
+          <li>
+            {listing.bathrooms > 1
+              ? `${listing.bathrooms} bathrooms`
+              : `${listing.bathrooms} bathroom`}
+          </li>
+          <li>{listing.parking ? "Parking Spot" : ""}</li>
+          <li>{listing.furnished ? "furnished" : ""}</li>
+        </ul>
+
+        <p className="listingLocationTitle">Location</p>
+
+        {/*MAP*/}
+
+        {auth.currentUser?.uid !== listing.userRef && (
+          <Link
+            to={`/contact/${listing.userRef}?listingName=${listing.name}&listingLocation=${listing.location}`}
+            className={"primaryButton"}
+          >
+            Contact Landlord
+          </Link>
+        )}
+      </div>
+    </main>
   );
 };
 
